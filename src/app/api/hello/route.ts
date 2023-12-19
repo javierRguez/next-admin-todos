46563e8a7038963ee37c9f5f8f16fdbcc857460c
JavierRodriguez <jotarodriguez939@gmail.com>
2023-12-19 14:23:51 +0100

    first commit & configure local postgress

---
+++ src/app/api/hello/route.ts
@@ -0,0 +1,8 @@
+// snippet ==> rag
+import { NextResponse, NextRequest } from "next/server";
+
+export async function GET(request: Request) {
+  return NextResponse.json({
+    hola: "mundo",
+  });
+}
