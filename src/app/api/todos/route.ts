771d00e94e3aad092f11f63fd0153f8d4f1a8c2a
JavierRodriguez <jotarodriguez939@gmail.com>
2023-12-20 12:07:26 +0100

    paginacion simple

--- src/app/api/todos/route.ts
+++ src/app/api/todos/route.ts
@@ -2,6 +2,26 @@ import prisma from "@/lib/prisma";
 import { NextResponse, NextRequest } from "next/server";
 
 export async function GET(request: Request) {
-  const todos = await prisma.todo.findMany();
+  const { searchParams } = new URL(request.url);
+  const take = +(searchParams.get("take") ?? "10"); //! Se utiliza '+' para parsear a numero el valor de take
+  const skip = +(searchParams.get("skip") ?? "0"); //! +() === Number()
+  if (isNaN(take)) {
+    return NextResponse.json(
+      { message: "Take tiene que ser un número" },
+      { status: 400 }
+    );
+  }
+
+  if (isNaN(skip)) {
+    return NextResponse.json(
+      { message: "Skip tiene que ser un número" },
+      { status: 400 }
+    );
+  }
+
+  const todos = await prisma.todo.findMany({
+    take,
+    skip,
+  });
   return NextResponse.json(todos);
 }
