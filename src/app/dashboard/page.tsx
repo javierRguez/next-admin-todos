ea029fac7f0a9ac3810854ef59b5676dd87f5260
JavierRodriguez <jotarodriguez939@gmail.com>
2023-12-21 14:01:01 +0100

    first app components

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,11 @@
+// snippet ==> prc
+
+import { WidgetItem } from "@/components";
+
+export default function DashboardPage() {
+  return (
+    <div className="grid gap-6 md:grid-cols-2 lg:grid-cols-3">
+      <WidgetItem title="Global Activities" />
+    </div>
+  );
+}
