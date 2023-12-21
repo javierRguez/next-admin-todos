65cbc6b48c32822a1d1c796065e898591aa747cc
JavierRodriguez <jotarodriguez939@gmail.com>
2023-12-21 18:45:47 +0100

    add first server action

--- src/todos/components/TodosGrid.tsx
+++ src/todos/components/TodosGrid.tsx
@@ -4,6 +4,7 @@ import { TodoItem } from "..";
 
 import * as todosApi from "@/todos/helpers/todos";
 import { useRouter } from "next/navigation";
+import { toggleTodo } from "../actions/todo-actions";
 
 interface Props {
   todos?: Todo[];
@@ -12,11 +13,12 @@ interface Props {
 export const TodosGrid = ({ todos = [] }: Props) => {
   const router = useRouter();
 
-  const toggleTodo = async (id: string, complete: boolean) => {
+  /*  const toggleTodo = async (id: string, complete: boolean) => {
     const updatedTodo = await todosApi.updateTodo(id, complete);
     router.refresh();
     return updatedTodo;
   };
+ */
 
   return (
     <div className="grid grid-cols-1 sm:grid-cols-3 gap-2">
