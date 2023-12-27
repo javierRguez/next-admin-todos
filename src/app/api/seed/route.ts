389c9ceffcfb4b44c729893fc572bf51fb52771d
JavierRodriguez <jotarodriguez939@gmail.com>
2023-12-27 23:06:19 +0100

    add credential Provider & add auth-actions & create relation btw Todo and User

--- src/app/api/seed/route.ts
+++ src/app/api/seed/route.ts
@@ -1,9 +1,28 @@
 import prisma from "@/lib/prisma";
 import { NextResponse, NextRequest } from "next/server";
+import bcrypt from "bcryptjs";
 
 export async function GET(request: Request) {
   await prisma.todo.deleteMany();
-  await prisma.todo.createMany({
+  await prisma.user.deleteMany();
+
+  const user = await prisma.user.create({
+    data: {
+      email: "test1@google.com",
+      password: bcrypt.hashSync("123456"),
+      roles: ["admin", "client", "super-user"],
+      todos: {
+        create: [
+          { description: "Piedra del alma", complete: true },
+          { description: "Piedra del poder" },
+          { description: "Piedra del tiempo" },
+          { description: "Piedra del espacio" },
+          { description: "Piedra de la realidad" },
+        ],
+      },
+    },
+  });
+  /*  await prisma.todo.createMany({
     data: [
       { description: "Piedra del alma", complete: true },
       { description: "Piedra del poder" },
@@ -11,7 +30,7 @@ export async function GET(request: Request) {
       { description: "Piedra del espacio" },
       { description: "Piedra de la realidad" },
     ],
-  });
+  }); */
 
   /* const todo = await prisma.todo.create({
     data: { description: "piedra del alma", complete: true },
