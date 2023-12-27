0c5c871929a1912e19d163487d1c0200eb4049b8
JavierRodriguez <jotarodriguez939@gmail.com>
2023-12-27 15:03:50 +0100

    get session data in Client and Server sides

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -7,8 +7,11 @@ import {
   IoCheckboxOutline,
   IoCodeWorkingOutline,
   IoListOutline,
+  IoPersonOutline,
 } from "react-icons/io5";
 import { SidebarItem } from "./SidebarItem";
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/app/api/auth/[...nextauth]/route";
 
 const sidebarItems = [
   {
@@ -36,9 +39,20 @@ const sidebarItems = [
     icon: <IoBasketOutline />,
     title: "Products",
   },
+  {
+    path: "/dashboard/profile",
+    icon: <IoPersonOutline />,
+    title: "Perfil",
+  },
 ];
 
-export const Sidebar = () => {
+export const Sidebar = async () => {
+  const session = await getServerSession(authOptions);
+  const avatarUrl = session?.user?.image
+    ? session.user.image
+    : "https://tailus.io/sources/blocks/stats-cards/preview/images/second_user.webp";
+  const userName = session?.user?.name ?? "No Name";
+
   return (
     <aside className="ml-[-100%] fixed z-10 top-0 pb-3 px-6 w-full flex flex-col justify-between h-screen border-r bg-white transition duration-300 md:w-4/12 lg:ml-0 lg:w-[25%] xl:w-[20%] 2xl:w-[15%]">
       <div>
@@ -55,14 +69,14 @@ export const Sidebar = () => {
         </div>
         <div className="mt-8 text-center">
           <Image
-            src="https://tailus.io/sources/blocks/stats-cards/preview/images/second_user.webp"
+            src={avatarUrl}
             alt=""
             width={100}
             height={100}
             className="w-10 h-10 m-auto rounded-full object-cover lg:w-28 lg:h-28"
           />
           <h5 className="hidden mt-4 text-xl font-semibold text-gray-600 lg:block">
-            Cynthia J. Watts
+            {userName}
           </h5>
           <span className="hidden text-gray-400 lg:block">Admin</span>
         </div>
